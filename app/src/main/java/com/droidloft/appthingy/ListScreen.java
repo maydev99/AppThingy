d2119f861cefb2e11d162fd66673626bd83e0758
Michael May <maydev99@gmail.com>
2016-12-21 11:11:41 -0500

    Implemented Cloud Backup and Restore

--- app/src/main/java/com/droidloft/appthingy/ListScreen.java
+++ app/src/main/java/com/droidloft/appthingy/ListScreen.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,7 +16,14 @@ import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by DroidLoft2 on 12/19/2016.
@@ -30,6 +38,13 @@ public class ListScreen extends AppCompatActivity {
     private  ArrayList<String>rowList;
     private ArrayAdapter<String> mainArrayAdapter;
     private Cursor cursor;
+    private List retrieveList;
+    boolean isRestoreList = false;
+    private String date2, name2, phone2;
+
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference listRef = rootRef.child("listref");
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +61,7 @@ public class ListScreen extends AppCompatActivity {
     }
 
     private void getTheData() {
+
         mainArrayList = new ArrayList<>();
         rowList = new ArrayList<>();
         mainArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_item_item, mainArrayList);
@@ -56,9 +72,9 @@ public class ListScreen extends AppCompatActivity {
         int phoneColumn = cursor.getColumnIndex("phone");
         cursor.moveToFirst();
 
-        if(cursor == null && cursor.getCount() == 0) {
+        /*if(cursor == null && cursor.getCount() == 0) {
             mainArrayAdapter.notifyDataSetChanged();
-        }
+        }*/
 
 
         if(cursor != null && cursor.getCount() > 0) {
@@ -115,11 +131,11 @@ public class ListScreen extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.backup) {
-
+            backupDataToCloud();
         }
 
         if(item.getItemId() == R.id.restore) {
-
+            restoreDataFromCloud();
         }
 
         if(item.getItemId() == R.id.gen_data) {
@@ -130,6 +146,65 @@ public class ListScreen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void restoreDataFromCloud() {
+        listRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                sqlDB.execSQL("DELETE FROM mytable");
+                retrieveList = new ArrayList();
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    retrieveList.add((String.valueOf(dsp.getValue())));
+
+                }
+                Log.d("Retrieved Data: " , "DATA: " + retrieveList);
+
+                int totalCount = 0;
+                int count = 0;
+                for(int i = 0; i < retrieveList.size(); i++){
+
+                    if(count < 4) {
+                        String text = (String) retrieveList.get(i);
+                        if(count == 0){
+                            date2 = text;
+                        }
+
+                        if(count == 1) {
+                            name2 = text;
+                        }
+
+                        if(count == 2) {
+                            phone2 = text;
+                            count = -1;
+                            Log.d("DATA BLOCKS: ", "DATA BLOCKS " + date2 + "" + "\n" + name2 + "\n" + phone2);
+                            sqlDB.execSQL("INSERT INTO mytable (date, name, phone) VALUES('" + date2 + "','" + name2 + "','" + phone2 + "');");
+                        }
+
+                        count++;
+                    }
+
+                }
+
+                getTheData();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void backupDataToCloud() {
+        listRef.removeValue();
+        for(int i = 0; i < mainArrayList.size(); i++) {
+            String item = mainArrayList.get(i);
+            listRef.push().setValue(item);
+        }
+    }
+    
+    
+    //Remove This Later
     private void generateData() {
         String date1 = "12/22/2016";
         String name1 = "Michael";
